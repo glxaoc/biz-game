ea22499421466e1da988460c0a8156e8ff95c5c6
glxaoc <drobitkoivan@gmail.com>
2026-06-23 13:56:05 +0200

    Убрать из репо посторонние lottie-player/ и lottie-test/ (попали по ошибке), добавить в .gitignore

--- lottie-player/src/components/ui/container.tsx
+++
@@ -1,9 +0,0 @@
-import type { JSX } from "solid-js";
-
-export function CenteredContainer(props: { children: JSX.Element }) {
-  return (
-    <div class="absolute inset-0 flex items-center justify-center text-xxs text-muted-foreground">
-      {props.children}
-    </div>
-  );
-}
